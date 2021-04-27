fbeeaa40ccce06edf9970baeccc69ffbb553ab49
Alvaro J. Castro Rivadeneira <acastrorivad@umass.edu>
2021-04-26 20:18:45 -0400

    Final student update

--- lyman_j-Group 2 merging data 390E Jackson Lyman.R
+++ lyman_j-Group 2 merging data 390E Jackson Lyman.R
@@ -3,9 +3,8 @@
 #Import datasets from where you downloaded them. the path will be different
 ##Exposures and Outcomes
 
-confounders2015 <- read.csv("C:/Users/Owner/Dropbox/UMASS/Teaching/EnvEpi_Spring2021/Datasets/confounders2015.csv")
-("~/Downloads/confounders2015.csv")
-group2 <- read.csv("~/Downloads/group2.csv")
+confounders2015 <- read.csv("confounders2015.csv")
+group2 <- read.csv("group2.csv")
 
 names (group2)
 names (confounders2015)
@@ -15,14 +14,15 @@ data2 <- merge(group2,confounders2015,by="SEQN")
 #Check where are your variables of interest in the datasets: Remember you only want to keep your exposure, outcome, and confounders
 names(data2)
 #Creating a new dataset only with what you need. For instance your exposure of interest is URXCOTT, and is in column 3. your outcome of interest KIQ022 is in column 25, and your confounders are in columns 40 to 44. I also kept the IDs "SEQN" in column 1
-data.final=data2[,c(1,3,22,25,40:44)]
+data.final=data2[,c(1,3,25,40:44)]
 
 #Remove all missing data
 data.final=na.omit(data.final)
-
+names(data.final)
+write.csv(data.final, "lyman_j-group2-merged.csv")
 
 #ONLY For groups who have depression as an outcome, you will need to construct the sum of DPQ010 to DPQ100
 #Construct the sum for a depression total score
-names(data.final)
+
 #To construct the sum, instead of DPQ010 and DPQ100 below, put the rank of the column where these variables are situated (as done above to select the variables you need)
 data.final$depression <- rowSums(data.final[,c(DPQ010:DPQ100)])
